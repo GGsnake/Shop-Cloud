c641a637bcc21a8f2433e765836befb012b90799
GGsnake <ljp462693266@outlook.com>
2019-07-05 15:46:31 +0800

    集成Apollo

---
+++ service-sdk/src/main/java/com/example/servicesdk/eum/SDKConfig.java
@@ -0,0 +1,6 @@
+package com.example.servicesdk.eum;
+
+public interface SDKConfig {
+    String PDDKEY="48dcc8985c8e4838be2dea3aa9b6176f";
+      String PDDSECRET="62b91c07f697bce84beec4123b01f6e108d2fa38";
+}
--- service-sdk/src/main/java/com/example/servicesdk/utils/SDKClient.java
+++ service-sdk/src/main/java/com/example/servicesdk/utils/SDKClient.java
@@ -1,13 +1,25 @@
 package com.example.servicesdk.utils;
 
+import com.example.servicesdk.eum.SDKConfig;
 import com.pdd.pop.sdk.http.PopClient;
 import com.pdd.pop.sdk.http.PopHttpClient;
-import org.springframework.stereotype.Component;
 
-@Component
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class SDKClient {
-    public static PopClient getPopClient(){
-        PopClient client = new PopHttpClient("48dcc8985c8e4838be2dea3aa9b6176f", "62b91c07f697bce84beec4123b01f6e108d2fa38");
-        return client;
+    private static PopClient popClient;
+    public static PopClient getPopClient() {
+        if (popClient == null) {
+            Lock lock = new ReentrantLock(false);
+            try {
+                lock.lock();
+                popClient = new PopHttpClient(SDKConfig.PDDKEY, SDKConfig.PDDSECRET);
+                return popClient;
+            } finally {
+                lock.unlock();
+            }
+        }
+        return null;
     }
 }
